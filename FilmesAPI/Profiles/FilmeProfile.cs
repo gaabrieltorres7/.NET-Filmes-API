886372da4d616696674cf3017e6ee3ba8f5282cb
Gabriel Torres <playerjogador10@outlook.com>
2023-01-19 16:24:50 -0300

    first commit

---
+++ FilmesAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesAPI.DTOs;
+using FilmesAPI.Models;
+
+namespace FilmesApi.Profiles;
+
+public class FilmeProfile : Profile
+{
+    public FilmeProfile()
+    {
+        CreateMap<CreateFilmeDto, Filme>();
+        CreateMap<UpdateFilmeDto, Filme>();
+        CreateMap<Filme, UpdateFilmeDto>();
+        CreateMap<Filme, ReadFilmeDto>();
+    }
+
+}
