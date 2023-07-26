c89eb4a46213dbe680e0bff38b42c1125c5f4de3
Gabriel Torres <playerjogador10@outlook.com>
2023-07-25 22:45:46 -0300

    feat: implement automapper to deal with different fields

--- FilmesAPI/Profiles/CinemaProfile.cs
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -10,7 +10,9 @@ public class CinemaProfile : Profile
     public CinemaProfile()
     {
         CreateMap<CreateCinemaDTO, Cinema>();
-        CreateMap<Cinema, ReadCinemaDTO>();
+        CreateMap<Cinema, ReadCinemaDTO>().ForMember(cinemaDto => cinemaDto.Endereco, 
+            opt => opt.MapFrom(cinema => cinema.Endereco)); // para um membro de destino específico (ReadEnderecoDTO) quero que pegue do cinema 
+            // o endereço
         CreateMap<UpdateCinemaDTO, Cinema>();
     }
 
