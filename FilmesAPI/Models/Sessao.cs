50f77a029b487c47b1b32aaaebb6eece11af2def
Gabriel Torres <playerjogador10@outlook.com>
2023-07-27 19:58:14 -0300

    refactor: filmeId nulo

--- FilmesAPI/Models/Sessao.cs
+++ FilmesAPI/Models/Sessao.cs
@@ -7,8 +7,7 @@ namespace FilmesAPI.Models
     [Key]
     [Required]
     public int Id { get; set; }
-    [Required]
-    public int FilmeId { get; set; }
+    public int? FilmeId { get; set; }
     public virtual Filme Filme { get; set; }
     public int? CinemaId { get; set; }
     public virtual Cinema Cinema { get; set; }
