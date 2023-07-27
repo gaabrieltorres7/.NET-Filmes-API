ad19c30515be06614c72d190917314ea128280cd
Gabriel Torres <playerjogador10@outlook.com>
2023-07-26 21:43:41 -0300

    feat: create model sessao

---
+++ FilmesAPI/Models/Sessao.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+  public class Sessao
+  {
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    public int FilmeId { get; set; }
+    public virtual Filme Filme { get; set; }
+  }
+}
