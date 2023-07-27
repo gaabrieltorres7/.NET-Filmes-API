5cdeb885bca02981fc712f524a91436a8eac95f7
Gabriel Torres <playerjogador10@outlook.com>
2023-07-26 22:08:33 -0300

    refactor: ToList in RecuperaFilmes method

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -38,7 +38,7 @@ namespace FilmesAPI.Controllers
         public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0,
         [FromQuery] int take = 50)
         {
-            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take).ToList());
         }
 
 
