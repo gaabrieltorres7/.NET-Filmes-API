8160049cee8abc47bbb527a94e76c20fd54f5838
Gabriel Torres <playerjogador10@outlook.com>
2023-07-26 22:12:48 -0300

    feat: sessao e cinema migrations

--- FilmesAPI/Migrations/20230727005412_Sessao e Cinema.Designer.cs
+++ FilmesAPI/Migrations/20230727005412_Sessao e Cinema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FilmesApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FilmesAPI.Migrations
 {
     [DbContext(typeof(FilmeContext))]
-    partial class FilmeContextModelSnapshot : ModelSnapshot
+    [Migration("20230727005412_Sessao e Cinema")]
+    partial class SessaoeCinema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
